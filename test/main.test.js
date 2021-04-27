414f96c13005b8b980c291ec6b9311aa9270aeac
Donald Wu <yeukfei02@gmail.com>
2021-04-27 22:41:02 +0800

    feat: 🎸 add Donald Wu Token and add test case

--- test/main.test.js
+++ test/main.test.js
@@ -1,14 +1,30 @@
 const { expect } = require("chai");
 
-describe("Greeter", function() {
-  it("Should return the new greeting once it's changed", async function() {
+describe("main.test", function () {
+  it("Greeter Test", async function () {
     const Greeter = await ethers.getContractFactory("Greeter");
     const greeter = await Greeter.deploy("Hello, world!");
-    
+
     await greeter.deployed();
     expect(await greeter.greet()).to.equal("Hello, world!");
 
     await greeter.setGreeting("Hola, mundo!");
     expect(await greeter.greet()).to.equal("Hola, mundo!");
+  });
+
+  it("Token Test", async function () {
+    const [owner, addr1, addr2] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("Token");
+    const token = await Token.deploy();
+
+    const ownerBalance = await token.balanceOf(owner.address);
+    expect(await token.totalSupply()).to.equal(ownerBalance);
+
+    await token.transfer(addr1.address, 100);
+    expect(await token.balanceOf(addr1.address)).to.equal(100);
+
+    await token.transfer(addr2.address, 200);
+    expect(await token.balanceOf(addr2.address)).to.equal(200);
   });
 });
